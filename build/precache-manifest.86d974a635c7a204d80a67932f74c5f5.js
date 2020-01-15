4b5c827de8235358c8c1c897f1716b1cda9aa7e8
Praise2112 <oketola.praise@gmail.com>
2020-01-15 21:06:08 +0700

    added build folder

---
+++ build/precache-manifest.86d974a635c7a204d80a67932f74c5f5.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "01471b130cdde30c3b004bc41827173d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6b72a18e93296533a7bf",
+    "url": "/static/css/2.2567e47f.chunk.css"
+  },
+  {
+    "revision": "f5369392d9460faf3def",
+    "url": "/static/css/main.a26ebfcb.chunk.css"
+  },
+  {
+    "revision": "6b72a18e93296533a7bf",
+    "url": "/static/js/2.f4c257af.chunk.js"
+  },
+  {
+    "revision": "13dc0f6cf93b864108fa2a7b458ac176",
+    "url": "/static/js/2.f4c257af.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f5369392d9460faf3def",
+    "url": "/static/js/main.de0518df.chunk.js"
+  },
+  {
+    "revision": "33230cba694eebabb0e0",
+    "url": "/static/js/runtime-main.e3c111a4.js"
+  },
+  {
+    "revision": "4a95439ebed24a30065b7f22f0cc5e61",
+    "url": "/static/media/bg.4a95439e.svg"
+  },
+  {
+    "revision": "0c637bd058e5b3c768c6f69027c6df1e",
+    "url": "/static/media/bg2.0c637bd0.svg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
